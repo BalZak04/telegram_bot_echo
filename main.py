a1d37fb2d1ad8982182c2d437b39887f89b3439a
echo bolt <nikbal044@gmail.com>
2022-10-22 10:56:00 +0300

    echo bot

---
+++ main.py
@@ -0,0 +1,39 @@
+from telegram.ext import MessageHandler, Filters, Updater
+from telegram.ext import CommandHandler
+
+
+token = '5622593689:AAHvAPVYmpXVv3LOic-FWuQs33Te-mFiMG4'
+
+
+def echo(update, conext):
+    txt = update.message.text
+
+    if txt.lower() in ['привет', 'как дела', 'txt']:
+        txt = 'И тебе привет ххх'
+
+    update.message.reply_text(txt)
+
+
+def start(update, conext):
+    update.message.reply_text('Я устал\nДа-да-да')
+
+
+def help(update, conext):
+    update.message.reply_text('А Я устал\nНет-Нет-Нет')
+
+
+def main():
+    updater = Updater(token, use_context=True)
+    dp = updater.dispatcher
+    print('Бот запущен...')
+
+    dp.add_handler(CommandHandler('start', start))
+    dp.add_handler(CommandHandler('help', help))
+
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == '__main__':
+    main()
